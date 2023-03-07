45e7bf70a94c2b5bcf44fa22eb795e731285c308
zigggggger <m.zygmanowski24@gmail.com>
2023-03-07 17:23:53 +0100

    gf

--- Logowanie.php
+++ Logowanie.php
@@ -12,8 +12,9 @@
 <body>
     <?php
         $servername = "localhost";
-        $username = "admin";
+        $username = "root";
         $password = "";
+        $databaseName = "wakacyjny-czar";
     ?>
     <div class="main">
         
@@ -29,30 +30,52 @@
             <button class="logowanie">Logowanie</button>
             <button class="rejestracja">Rejestracja</button>
         </div>
-        <div class="panel-log">
-        <?php
-            $con = new mysqli($servername, $username, $password);
-            if(isset($_POST["submit"])){
-                if($_POST["password"] == "sekret1"){
-                ?>
-                Tutaj jest chroniona zawartość...
+        <div class="panel-rej">
                 <?php
+                    $Username = $_POST["Username"];
+                    $e_mail = $_POST["e-mail"];
+                    $Password = $_POST["password"];
+                    $con = new mysqli($servername, $username, $password);
+                    if(isset($_POST["submit"])){
+                        $sql = "INSERT INTO user (Username, e-mail, password) VALUES ('$Username', '$e_mail', '$Password')";
+                    }
+                ?>
+                <form action="POST">
+                    <input type="text" name="Username" placeholder="Nazwa użytkownika">
+                    <input type="text" name="e-mail" placeholder="Adres E-mail">
+                    <input type="password" name="password" id="pass1" placeholder="Hasło">
+                    <input type="password" id="pass2" placeholder="Powtórz hasło">
+                    <input type="text" name="Name" placeholder="Imię">
+                    <input type="text" name="Surname" placeholder="Nazwisko">
+                    <input type="text" name="City" placeholder="Miasto">
+                    <input type="text" name="Zip-code" placeholder="Kod pocztowy (XX-XXX)">
+                    <input type="text" name="Address" placeholder="Adres (Ulica i numer domu/numer mieszkania)">
+                    <input type="button" value="submit">
+                </form>
+        </div>
+        <!-- <div class="panel-log">
+            <?php
+                $con = new mysqli($servername, $username, $password);
+                if(isset($_POST["submit"])){
+                    if($_POST["password"] == "sekret1"){
+                    ?>
+                    Tutaj jest chroniona zawartość...
+                    <?php
+                    }
+                    else{
+                    echo "Nieprawidłowe hasło";
+                    }
                 }
                 else{
-                echo "Nieprawidłowe hasło";
+                echo "Wprowadź hasło";
                 }
-            }
-            else{
-            echo "Wprowadź hasło";
-            }
             ?>
             <form method="POST">
                 <input type="text" name="login" placeholder="Podaj nazwę użytkownika lub e-mail">
                 <input type="password" name="password" placeholder="Podaj hasło">
                 <input type="submit" name="submit" value="Wyślij">
             </form>
-        </div>
-        <div class="panel-rej"></div>
+        </div> -->
     </div>
 </body>
 </html>
