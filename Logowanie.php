98a8a52633d3642e222b66ca3d13814547b4b9bb
RilhS <127195855+RilhS@users.noreply.github.com>
2023-03-07 17:37:00 +0100

    dfg

--- Logowanie.php
+++ Logowanie.php
@@ -10,49 +10,24 @@
 
 </head>
 <body>
-    <?php
-        $servername = "localhost";
-        $username = "admin";
-        $password = "";
-    ?>
-    <div class="main">
+    <div id="main">
         
-        <div class="menu">
-            <div class="logo"><img class="Logo" src="img/favicon.png" alt=""> Wakacyjny czar</div>
-            <div class="Home">Home</div>
-            <div class="oferta">Oferta </div>
-            <div class="Onas">O nas</div>
-            <div class="Konto">Konto</div>
+        <div id="menu">
+            <div id="Logo"> <img id="Logo"#Logo src="img/napislogo.png" alt=""></div>
+            <div id="Home">Home</div>
+            <div id="oferta">Oferta </div>
+            <div id="Onas">O nas</div>
+            <div id="Konto">Konto</div>
 
         </div>
-        <div class="logowanie">
-            <button class="logowanie">Logowanie</button>
-            <button class="rejestracja">Rejestracja</button>
+        <div id="logowanie">
+            
+            <button id="logowanie">Logowanie</button>
+            <button id="rejestracja">Rejestracja</button>
         </div>
-        <div class="panel-log">
-        <?php
-            $con = new mysqli($servername, $username, $password);
-            if(isset($_POST["submit"])){
-                if($_POST["password"] == "sekret1"){
-                ?>
-                Tutaj jest chroniona zawartość...
-                <?php
-                }
-                else{
-                echo "Nieprawidłowe hasło";
-                }
-            }
-            else{
-            echo "Wprowadź hasło";
-            }
-            ?>
-            <form method="POST">
-                <input type="text" name="login" placeholder="Podaj nazwę użytkownika lub e-mail">
-                <input type="password" name="password" placeholder="Podaj hasło">
-                <input type="submit" name="submit" value="Wyślij">
-            </form>
+        <div>
+
         </div>
-        <div class="panel-rej"></div>
     </div>
 </body>
 </html>
