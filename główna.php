df2179529ac2db5e3205cff8fffaf0bb372cafec
RilhS <127195855+RilhS@users.noreply.github.com>
2023-03-08 12:33:07 +0100

    sadasadasd

--- główna.php
+++ główna.php
@@ -10,7 +10,20 @@
 </head>
 <body>
 <div class="main">
-
+    <div class="menu">
+        <div class="Logo"> <img id="logo"#Logo src="img/napislogo1.png" alt=""></div>
+        <hr class="Blank">
+        <div class="Home">Home</div>
+        <div class="oferta">Oferta </div>
+        <div class="Onas">O nas
+            <div class="Onas1">Polityka prywatności</div>
+            <div class="Onas1">O nas</div>
+        </div>
+        <div class="Konto">Konto
+            <div class="Konto1">Moje wycieczki</div>
+            <div class="Konto1">Ustawienia</div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
