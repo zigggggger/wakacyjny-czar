7ec5aab2a32a253a73161c3d353ffa0f5eb55d23
zigggggger <m.zygmanowski24@gmail.com>
2023-03-09 11:40:00 +0100

    sdf

--- Logowanie.php
+++ Logowanie.php
@@ -9,6 +9,12 @@
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
 </head>
 <body>
+    <?php
+        $servername = "localhost";
+        $usrnme = "root";
+        $passwd = "";
+        $databaseName = "wakacyjny-czar";
+    ?>
     <div class="main">
         <div class="menu">
             <div class="Logo"> <img id="logo"#Logo src="img/napislogo1.png" alt=""></div>
@@ -36,7 +42,7 @@
             <button id="plogowanie" onclick="pokazL()">Logowanie</button> 
             <button id="prejestracja" onclick="pokazR()">Rejestracja</button></div>
             <div id="panel-rejestracja">
-                <form class="panel-rejestracja" action="POST">
+                <form class="panel-rejestracja" method="POST" action="Logowanie.php">
                     <input class="panel" type="text" name="Username" placeholder="Nazwa użytkownika">
                     <input class="panel" type="text" name="e-mail" placeholder="Adres E-mail">
                     <input class="panel" type="password" name="password" id="pass1" placeholder="Hasło">
@@ -46,8 +52,62 @@
                     <input class="panel" type="text" name="City" placeholder="Miasto">
                     <input class="panel" type="text" name="Zip-code" placeholder="Kod pocztowy (XX-XXX)">
                     <input class="panel" type="text" name="Address" placeholder="Adres (Ulica i numer domu/numer mieszkania)"><br>
-                    <input class="panel-zaj" type="button" value="Zajerestruj">
+                    <input class="panel-zaj" type="submit" name="submit" value="Zajerestruj">
                 </form>
+                <?php
+                    $con = new mysqli($servername, $usrnme, $passwd, $databaseName);
+                    if(isset($_POST["submit"])){
+                        if (!empty($_POST['Username']) && !empty($_POST['e-mail'])){ 
+                            $Username = $_POST["Username"];
+                            $e_mail = $_POST["e-mail"];
+                            $Password = $_POST["password"];
+                            $Name = $_POST["Name"];
+                            $Surname = $_POST["Surname"];
+                            $City = $_POST["City"];
+                            $Zip_code = $_POST["Zip-code"];
+                            $Address = $_POST["Address"];
+                        }
+                        $sql1 = "INSERT INTO `clients` (`ID`, `Name`, `Surname`, `City`, `Zip_code`, `Address`) VALUES (NULL, '$Name', '$Surname', '$City', '$Zip_code', '$Address')";
+                        if(isset($sql1)){
+                            if ($con->query($sql1) === TRUE) {
+                                echo "New record created successfully";
+                            } 
+                            else {
+                                echo "Error: " . $sql1 . "<br>" . $con->error;
+                            }
+                        }
+                        $sql3 = "SELECT ID FROM clients WHERE `Name` = '$Name' AND `Surname` = '$Surname' AND `City` = '$City' AND `Zip_code` = '$Zip_code' AND `Address` = '$Address';";
+                        // if(isset($sql3)){
+                        //     if ($result = $con->query($sql3) === TRUE) {
+                        //         echo "New record created successfully";
+                        //     } 
+                        //     else {
+                        //         echo "Error: " . $sql3 . "<br>" . $con->error;
+                        //     }
+                        // }
+                        $result = $con->query($sql3);
+                        $result = $result->fetch_assoc();
+                        echo $result;
+                        $result = $result['ID'];
+                        $sql2 = "INSERT INTO `user` (`ID`, `Username`, `e-mail`, `Password`, `ID_client`) VALUES ($result3, '$Username', '$e_mail' , '$Password')";
+                    }
+                    if ($con->connect_error) {
+                        die("Connection failed: " . $con->connect_error);
+                    }
+                    else{
+                        echo "powinno śmigać";
+                    }
+                    if(isset($sql2)){
+                        if ($con->query($sql2) === TRUE) {
+                            echo "New record created successfully";
+                        } 
+                        else {
+                            echo "Error: " . $sql2 . "<br>" . $con->error;
+                        }
+                    }
+                    $con->close();
+                    
+                ?>
             </div>
             <div id="panel-logowanie">
                 <form class="panel-logowanie" method="POST">
